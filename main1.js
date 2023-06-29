c7f3a6f19bdcd46ed3f894803117df063fe8520c
EC2 Default User <ec2-user@ip-172-31-10-3.us-east-2.compute.internal>
2023-06-29 15:36:10 +0000

    小数点が連続で入力されないようにするための修正

--- main1.js
+++ main1.js
@@ -2,6 +2,8 @@
   
   let  is_calc = false;　//　=で計算したかどうか
   
+ 
+  
   window.onload = function () {
   result = document.getElementById('result');
 };
@@ -15,6 +17,7 @@ function num_click(val){ 　　//数字キー
    if(is_calc)  result.value = "0";
    is_calc = false;
 
+
 if(result.value == "0" &&val =="0"){
    result.value ="0";
   }else if(result.value =="0" && val=="."){
@@ -25,6 +28,7 @@ if(result.value == "0" &&val =="0"){
         result.value+=val;
     }
 }
+
  
  function ope_click(val){
      if(is_calc){
@@ -44,7 +48,30 @@ if(result.value == "0" &&val =="0"){
     }
     
     function is_ope_last(){
-  return ["+","-","*","/"].includes(result.value.toString().slice(-1));
+  return ["+","-","*","/" ].includes(result.value.toString().slice(-1));
 }
-     
-     
+    
+    function shosu_click(val){ 　　//数字キー
+   if(is_calc)  result.value = "0";
+   is_calc = false;
+
+if(result.value == "0" &&val =="0"){
+   result.value ="0";
+  }else if(result.value =="0" && val=="."){
+    result.value ="0.";
+  }else if(result.value =="0"){
+    result.value=val;
+    }else{
+        result.value+=val;
+    }
+  }
+  
+ function shosu_click(val){
+   result.value += "."; 
+	document.getElementById("output").innerHTML = result.value;
+}
+
+function shosu_click(val)  { 
+	if (result.value.indexOf(".") < 0) result.value+= "."; 
+	document.getElementById("output").innerHTML = result.value;
+}
