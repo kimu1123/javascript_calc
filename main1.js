51b993f73b9857dd0749d1c65213487fb9a9340b
EC2 Default User <ec2-user@ip-172-31-10-3.us-east-2.compute.internal>
2023-06-29 10:23:34 +0000

    実践課題２　電卓の提出

---
+++ main1.js
@@ -0,0 +1,50 @@
+  let result = ""; //データ
+  
+  let  is_calc = false;　//　=で計算したかどうか
+  
+  window.onload = function () {
+  result = document.getElementById('result');
+};
+
+function c_click(){　　　　//関数宣言　ACキー押下
+result.value = "0";
+  is_calc = false;
+}
+  
+function num_click(val){ 　　//数字キー
+   if(is_calc)  result.value = "0";
+   is_calc = false;
+
+if(result.value == "0" &&val =="0"){
+   result.value ="0";
+  }else if(result.value =="0" && val=="."){
+    result.value ="0.";
+}else if(result.value =="0"){
+    result.value=val;
+    }else{
+        result.value+=val;
+    }
+}
+ 
+ function ope_click(val){
+     if(is_calc){
+         is_calc =false;
+     }
+     if(is_ope_last()){
+          result.value = result.value.slice(0, -1) + val;
+     } else {
+    result.value += val;
+     }
+}
+ 
+ function equal_click(){
+       let temp = eval(result.value);
+        result.value = temp;
+        is_calc = true;
+    }
+    
+    function is_ope_last(){
+  return ["+","-","*","/"].includes(result.value.toString().slice(-1));
+}
+     
+     
